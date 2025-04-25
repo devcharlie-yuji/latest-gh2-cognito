60b5b9e75cd6ef9815e2e54eb5439debb9257c3a
devcharlier <devcharlie@yuji.co.in>
2025-04-25 23:22:20 +0530

    update index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+// Imports at the top (before any other code)
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Amplify setup
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+
+// Amplify Auth UI
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+// Redux-related imports
 import { routerMiddleware } from 'connected-react-router';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
@@ -8,22 +20,12 @@ import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
 import App from './components/App';
-
 import createRootReducer from './reducers';
 import { doInit } from './actions/auth';
 import { createHashHistory } from 'history';
 
-// Amplify setup
-import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+// Amplify configuration
 Amplify.configure(config);
-
-// Amplify Auth UI
-import '@aws-amplify/ui-react/styles.css';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-
-
-
 
 const history = createHashHistory();
 
@@ -65,6 +67,76 @@ root.render(
 
 // Service worker
 serviceWorker.unregister();
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { routerMiddleware } from 'connected-react-router';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import { Provider } from 'react-redux';
+// import ReduxThunk from 'redux-thunk';
+// import * as serviceWorker from './serviceWorker';
+// import axios from 'axios';
+
+// import App from './components/App';
+
+// import createRootReducer from './reducers';
+// import { doInit } from './actions/auth';
+// import { createHashHistory } from 'history';
+
+// // Amplify setup
+// import { Amplify } from 'aws-amplify';
+// import config from './aws-exports';
+// Amplify.configure(config);
+
+// // Amplify Auth UI
+// import '@aws-amplify/ui-react/styles.css';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+
+
+
+
+// const history = createHashHistory();
+
+// export function getHistory() {
+//     return history;
+// }
+
+// // Set Axios defaults
+// axios.defaults.baseURL = config.baseURLApi;
+// axios.defaults.headers.common['Content-Type'] = "application/json";
+// const token = localStorage.getItem('token');
+// if (token) {
+//     axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+// }
+
+// export const store = createStore(
+//     createRootReducer(history),
+//     compose(
+//         applyMiddleware(
+//             routerMiddleware(history),
+//             ReduxThunk
+//         ),
+//     )
+// );
+
+// // Init Redux auth state
+// store.dispatch(doInit());
+
+// // Wrap your App with Amplify Authenticator
+// const AuthenticatedApp = withAuthenticator(App);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//     <Provider store={store}>
+//         <AuthenticatedApp />
+//     </Provider>
+// );
+
+// // Service worker
+// serviceWorker.unregister();
 
 
 
