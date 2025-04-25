75816c076c6fe7cb767bf7a65ae903b8635f2185
devcharlier <devcharlie@yuji.co.in>
2025-04-25 22:48:48 +0530

    add .cognito auth functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
-import { connect } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import {
   Navbar,
@@ -21,6 +21,13 @@ import {
   Button,
   Form
 } from 'reactstrap';
+import {
+  openSidebar,
+  closeSidebar,
+  changeSidebarPosition,
+  changeSidebarVisibility
+} from '../../actions/navigation';
+
 import Notifications from '../Notifications';
 import PowerIcon from "../Icons/HeaderIcons/PowerIcon";
 import BellIcon from "../Icons/HeaderIcons/BellIcon";
@@ -28,37 +35,22 @@ import SettingsIcon from "../Icons/HeaderIcons/SettingsIcon";
 import MessageIcon from "../Icons/HeaderIcons/MessageIcon";
 import BurgerIcon from "../Icons/HeaderIcons/BurgerIcon";
 import SearchIcon from "../Icons/HeaderIcons/SearchIcon";
-import ArrowIcon from "../Icons/HeaderIcons/ArrowIcon";
-
-
-import { logoutUser } from '../../actions/auth';
-import { openSidebar, closeSidebar, changeSidebarPosition, changeSidebarVisibility } from '../../actions/navigation';
-
-import sender1 from '../../images/people/a1.jpg';
-import sender2 from '../../images/people/a5.jpg';
-import sender3 from '../../images/people/a4.jpg';
 
 import adminDefault from '../../images/chat/chat2.png';
-
 import s from './Header.module.scss';
+
+import { withAuthenticator } from "@aws-amplify/ui-react"; // ✅ Add this
 
 class Header extends React.Component {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
     sidebarPosition: PropTypes.string.isRequired,
+    signOut: PropTypes.func, // ✅ Cognito signOut function
+    currentUser: PropTypes.object,
   };
 
   constructor(props) {
     super(props);
-
-    this.doLogout = this.doLogout.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
-    this.toggleMessagesDropdown = this.toggleMessagesDropdown.bind(this);
-    this.toggleSupportDropdown = this.toggleSupportDropdown.bind(this);
-    this.toggleSettingsDropdown = this.toggleSettingsDropdown.bind(this);
-    this.toggleAccountDropdown = this.toggleAccountDropdown.bind(this);
-    this.toggleSidebar = this.toggleSidebar.bind(this);
-    this.toggleSearchOpen = this.toggleSearchOpen.bind(this);
 
     this.state = {
       visible: true,
@@ -67,74 +59,61 @@ class Header extends React.Component {
       settingsOpen: false,
       searchFocused: false,
       searchOpen: false,
-      notificationsOpen: false
+      notificationsOpen: false,
     };
   }
 
+  doLogout = async () => {
+    try {
+      await this.props.signOut(); // ✅ This will trigger Cognito sign-out and redirect
+    } catch (error) {
+      console.error("Logout error:", error);
+    }
+  }
+
   toggleNotifications = () => {
-    this.setState({
-      notificationsOpen: !this.state.notificationsOpen,
-    });
+    this.setState(prev => ({ notificationsOpen: !prev.notificationsOpen }));
   }
 
-  onDismiss() {
-    this.setState({ visible: false });
+  toggleMessagesDropdown = () => {
+    this.setState(prev => ({ messagesOpen: !prev.messagesOpen }));
   }
 
-  doLogout() {
-    this.props
-      .dispatch(logoutUser());
+  toggleSupportDropdown = () => {
+    this.setState(prev => ({ supportOpen: !prev.supportOpen }));
   }
 
-  toggleMessagesDropdown() {
-    this.setState({
-      messagesOpen: !this.state.messagesOpen,
-    });
+  toggleSettingsDropdown = () => {
+    this.setState(prev => ({ settingsOpen: !prev.settingsOpen }));
   }
 
-  toggleSupportDropdown() {
-    this.setState({
-      supportOpen: !this.state.supportOpen,
-    });
+  toggleAccountDropdown = () => {
+    this.setState(prev => ({ accountOpen: !prev.accountOpen }));
   }
 
-  toggleSettingsDropdown() {
-    this.setState({
-      settingsOpen: !this.state.settingsOpen,
-    });
+  toggleSearchOpen = () => {
+    this.setState(prev => ({ searchOpen: !prev.searchOpen }));
   }
 
-  toggleAccountDropdown() {
-    this.setState({
-      accountOpen: !this.state.accountOpen,
-    });
-  }
-
-  toggleSearchOpen() {
-    this.setState({
-      searchOpen: !this.state.searchOpen,
-    });
-  }
-
-  toggleSidebar() {
+  toggleSidebar = () => {
     this.props.isSidebarOpened
       ? this.props.dispatch(closeSidebar())
-      : this.props.dispatch(openSidebar())
+      : this.props.dispatch(openSidebar());
   }
 
-  moveSidebar(position) {
+  moveSidebar = (position) => {
     this.props.dispatch(changeSidebarPosition(position));
   }
 
-  toggleVisibilitySidebar(visibility) {
+  toggleVisibilitySidebar = (visibility) => {
     this.props.dispatch(changeSidebarVisibility(visibility));
   }
 
   render() {
     const user = this.props.currentUser;
-    const avatar = user && user.avatar && user.avatar.length && user.avatar[0].publicUrl;
+    const avatar = user?.avatar?.length && user.avatar[0].publicUrl;
+    const firstUserLetter = user && (user.firstName || user.email)[0].toUpperCase();
 
-    const firstUserLetter = user && (user.firstName|| user.email)[0].toUpperCase();
     return (
       <Navbar className="d-print-none">
         <div className={s.burger}>
@@ -218,9 +197,9 @@ class Header extends React.Component {
             </Dropdown>
 
             <NavItem>
-              <NavLink onClick={this.doLogout} className={`${s.navItem} text-white`} href="#">
+              <Button onClick={this.doLogout} style={{ color: 'white', backgroundColor: 'transparent', border: 'none' }}>
                 <PowerIcon className={s.headerIcon} />
-              </NavLink>
+              </Button>
             </NavItem>
           </Nav>
         </div>
@@ -238,7 +217,253 @@ function mapStateToProps(store) {
   };
 }
 
-export default withRouter(connect(mapStateToProps)(Header));
+// ✅ Wrapped with withAuthenticator to get signOut from Cognito
+export default withAuthenticator(withRouter(connect(mapStateToProps)(Header)));
+
+
+
+
+// import { connect } from 'react-redux';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { withRouter } from 'react-router';
+// import {
+//   Navbar,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   InputGroupText,
+//   InputGroup,
+//   Input,
+//   UncontrolledAlert,
+//   Dropdown,
+//   Collapse,
+//   DropdownToggle,
+//   DropdownMenu,
+//   DropdownItem,
+//   Badge,
+//   ButtonGroup,
+//   Button,
+//   Form
+// } from 'reactstrap';
+// import Notifications from '../Notifications';
+// import PowerIcon from "../Icons/HeaderIcons/PowerIcon";
+// import BellIcon from "../Icons/HeaderIcons/BellIcon";
+// import SettingsIcon from "../Icons/HeaderIcons/SettingsIcon";
+// import MessageIcon from "../Icons/HeaderIcons/MessageIcon";
+// import BurgerIcon from "../Icons/HeaderIcons/BurgerIcon";
+// import SearchIcon from "../Icons/HeaderIcons/SearchIcon";
+// import ArrowIcon from "../Icons/HeaderIcons/ArrowIcon";
+
+
+// import { logoutUser } from '../../actions/auth';
+// import { openSidebar, closeSidebar, changeSidebarPosition, changeSidebarVisibility } from '../../actions/navigation';
+
+// import sender1 from '../../images/people/a1.jpg';
+// import sender2 from '../../images/people/a5.jpg';
+// import sender3 from '../../images/people/a4.jpg';
+
+// import adminDefault from '../../images/chat/chat2.png';
+
+// import s from './Header.module.scss';
+
+// class Header extends React.Component {
+//   static propTypes = {
+//     dispatch: PropTypes.func.isRequired,
+//     sidebarPosition: PropTypes.string.isRequired,
+//   };
+
+//   constructor(props) {
+//     super(props);
+
+//     this.doLogout = this.doLogout.bind(this);
+//     this.onDismiss = this.onDismiss.bind(this);
+//     this.toggleMessagesDropdown = this.toggleMessagesDropdown.bind(this);
+//     this.toggleSupportDropdown = this.toggleSupportDropdown.bind(this);
+//     this.toggleSettingsDropdown = this.toggleSettingsDropdown.bind(this);
+//     this.toggleAccountDropdown = this.toggleAccountDropdown.bind(this);
+//     this.toggleSidebar = this.toggleSidebar.bind(this);
+//     this.toggleSearchOpen = this.toggleSearchOpen.bind(this);
+
+//     this.state = {
+//       visible: true,
+//       messagesOpen: false,
+//       supportOpen: false,
+//       settingsOpen: false,
+//       searchFocused: false,
+//       searchOpen: false,
+//       notificationsOpen: false
+//     };
+//   }
+
+//   toggleNotifications = () => {
+//     this.setState({
+//       notificationsOpen: !this.state.notificationsOpen,
+//     });
+//   }
+
+//   onDismiss() {
+//     this.setState({ visible: false });
+//   }
+
+//   doLogout() {
+//     this.props
+//       .dispatch(logoutUser());
+//   }
+
+//   toggleMessagesDropdown() {
+//     this.setState({
+//       messagesOpen: !this.state.messagesOpen,
+//     });
+//   }
+
+//   toggleSupportDropdown() {
+//     this.setState({
+//       supportOpen: !this.state.supportOpen,
+//     });
+//   }
+
+//   toggleSettingsDropdown() {
+//     this.setState({
+//       settingsOpen: !this.state.settingsOpen,
+//     });
+//   }
+
+//   toggleAccountDropdown() {
+//     this.setState({
+//       accountOpen: !this.state.accountOpen,
+//     });
+//   }
+
+//   toggleSearchOpen() {
+//     this.setState({
+//       searchOpen: !this.state.searchOpen,
+//     });
+//   }
+
+//   toggleSidebar() {
+//     this.props.isSidebarOpened
+//       ? this.props.dispatch(closeSidebar())
+//       : this.props.dispatch(openSidebar())
+//   }
+
+//   moveSidebar(position) {
+//     this.props.dispatch(changeSidebarPosition(position));
+//   }
+
+//   toggleVisibilitySidebar(visibility) {
+//     this.props.dispatch(changeSidebarVisibility(visibility));
+//   }
+
+//   render() {
+//     const user = this.props.currentUser;
+//     const avatar = user && user.avatar && user.avatar.length && user.avatar[0].publicUrl;
+
+//     const firstUserLetter = user && (user.firstName|| user.email)[0].toUpperCase();
+//     return (
+//       <Navbar className="d-print-none">
+//         <div className={s.burger}>
+//           <NavLink onClick={this.toggleSidebar} className={`d-md-none ${s.navItem} text-white`} href="#">
+//             <BurgerIcon className={s.headerIcon} />
+//           </NavLink>
+//         </div>
+
+//         <div className={`d-print-none ${s.root}`}>
+//           <UncontrolledAlert className="me-3 d-lg-down-none animate__animated animate__bounceIn animate__delay-1s">
+//             Check out Light Blue
+//             <button
+//               className="btn-link"
+//               onClick={() => this.setState({ settingsOpen: true })}
+//             >
+//               <SettingsIcon className={`${s.settingsIcon} btn-link`} />
+//             </button>
+//             on the right!
+//           </UncontrolledAlert>
+
+//           <Collapse className={`${s.searchCollapse} ms-lg-0 me-md-3`} isOpen={this.state.searchOpen}>
+//             <InputGroup className={`${s.navbarForm} ${this.state.searchFocused ? s.navbarFormFocused : ''}`}>
+//               <InputGroupText className={s.inputAddon}>
+//                 <i className="fa fa-search" />
+//               </InputGroupText>
+//               <Input
+//                 id="search-input-2"
+//                 placeholder="Search..."
+//                 className="input-transparent"
+//                 onFocus={() => this.setState({ searchFocused: true })}
+//                 onBlur={() => this.setState({ searchFocused: false })}
+//               />
+//             </InputGroup>
+//           </Collapse>
+
+//           <Form className="d-md-down-none mx-3 my-auto">
+//             <InputGroup className={`input-group-no-border ${s.searchForm}`}>
+//               <InputGroupText className={s.inputGroupText}>
+//                 <SearchIcon className={s.headerIcon} />
+//               </InputGroupText>
+//               <Input id="search-input" className="input-transparent" placeholder="Search Dashboard" />
+//             </InputGroup>
+//           </Form>
+
+//           <Nav className="ms-md-0">
+//             <Dropdown nav isOpen={this.state.notificationsOpen} toggle={this.toggleNotifications}>
+//               <DropdownToggle nav caret style={{ color: "#C1C3CF", padding: 0 }}>
+//                 <span className={`small d-sm-down-none ${s.adminEmail}`}>{user ? (user.firstName || user.email) : "User"}</span>
+//                 <Badge className={`d-sm-down-none ${s.badge}`} color="danger">9</Badge>
+//               </DropdownToggle>
+//               <DropdownMenu className={`${s.notificationsWrapper} py-0`}>
+//                 <Notifications />
+//               </DropdownMenu>
+//             </Dropdown>
+
+//             <Dropdown nav isOpen={this.state.messagesOpen} toggle={this.toggleMessagesDropdown}>
+//               <DropdownToggle nav className={`d-sm-down-none ${s.navItem} text-white`}>
+//                 <MessageIcon className={s.headerIcon} />
+//               </DropdownToggle>
+//               <DropdownMenu className={`${s.dropdownMenu} ${s.messages}`}>
+//                 <DropdownItem>Messages loading...</DropdownItem>
+//               </DropdownMenu>
+//             </Dropdown>
+
+//             <Dropdown nav isOpen={this.state.settingsOpen} toggle={this.toggleSettingsDropdown}>
+//               <DropdownToggle nav className={`${s.navItem} text-white`}>
+//                 <SettingsIcon className={s.headerIcon} />
+//               </DropdownToggle>
+//               <DropdownMenu className={`${s.dropdownMenu} ${s.settings}`}>
+//                 <h6>Sidebar on the</h6>
+//                 <ButtonGroup size="sm">
+//                   <Button onClick={() => this.moveSidebar('left')} className={this.props.sidebarPosition === 'left' ? 'active' : ''}>Left</Button>
+//                   <Button onClick={() => this.moveSidebar('right')} className={this.props.sidebarPosition === 'right' ? 'active' : ''}>Right</Button>
+//                 </ButtonGroup>
+//                 <h6 className="mt-2">Sidebar</h6>
+//                 <ButtonGroup size="sm">
+//                   <Button onClick={() => this.toggleVisibilitySidebar('show')} className={this.props.sidebarVisibility === 'show' ? 'active' : ''}>Show</Button>
+//                   <Button onClick={() => this.toggleVisibilitySidebar('hide')} className={this.props.sidebarVisibility === 'hide' ? 'active' : ''}>Hide</Button>
+//                 </ButtonGroup>
+//               </DropdownMenu>
+//             </Dropdown>
+
+//             <NavItem>
+//               <NavLink onClick={this.doLogout} className={`${s.navItem} text-white`} href="#">
+//                 <PowerIcon className={s.headerIcon} />
+//               </NavLink>
+//             </NavItem>
+//           </Nav>
+//         </div>
+//       </Navbar>
+//     );
+//   }
+// }
+
+// function mapStateToProps(store) {
+//   return {
+//     isSidebarOpened: store.navigation.sidebarOpened,
+//     sidebarVisibility: store.navigation.sidebarVisibility,
+//     sidebarPosition: store.navigation.sidebarPosition,
+//     currentUser: store.auth.currentUser,
+//   };
+// }
+
+// export default withRouter(connect(mapStateToProps)(Header));
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { routerMiddleware } from 'connected-react-router';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux'
-import ReduxThunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
 import App from './components/App';
-import config from './config';
+
 import createRootReducer from './reducers';
 import { doInit } from './actions/auth';
 import { createHashHistory } from 'history';
+
+// Amplify setup
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
+// Amplify Auth UI
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+
+
 
 const history = createHashHistory();
 
@@ -19,6 +31,7 @@ export function getHistory() {
     return history;
 }
 
+// Set Axios defaults
 axios.defaults.baseURL = config.baseURLApi;
 axios.defaults.headers.common['Content-Type'] = "application/json";
 const token = localStorage.getItem('token');
@@ -36,17 +49,210 @@ export const store = createStore(
     )
 );
 
+// Init Redux auth state
 store.dispatch(doInit());
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+// Wrap your App with Amplify Authenticator
+const AuthenticatedApp = withAuthenticator(App);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider store={store}>
-        <App />
-    </Provider>,
+        <AuthenticatedApp />
+    </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Service worker
 serviceWorker.unregister();
+
+
+
+// // Import all dependencies first, before anything else
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { routerMiddleware } from 'connected-react-router';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import { Provider } from 'react-redux';
+// import ReduxThunk from 'redux-thunk';
+// import * as serviceWorker from './serviceWorker';
+// import axios from 'axios';
+
+// import App from './components/App';
+// import createRootReducer from './reducers';
+// import { doInit } from './actions/auth';
+// import { createHashHistory } from 'history';
+
+// // Amplify setup imports
+// import { Amplify } from 'aws-amplify';
+// import config from './aws-exports';
+// import '@aws-amplify/ui-react/styles.css';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+
+// // Setting up the history and axios defaults (these go after the imports)
+// const history = createHashHistory();
+
+// export function getHistory() {
+//     return history;
+// }
+
+// // Set Axios defaults
+// axios.defaults.baseURL = config.baseURLApi;
+// axios.defaults.headers.common['Content-Type'] = "application/json";
+// const token = localStorage.getItem('token');
+// if (token) {
+//     axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+// }
+
+// // Create Redux store and apply middlewares
+// export const store = createStore(
+//     createRootReducer(history),
+//     compose(
+//         applyMiddleware(
+//             routerMiddleware(history),
+//             ReduxThunk
+//         ),
+//     )
+// );
+
+// // Init Redux auth state
+// store.dispatch(doInit());
+
+// // Wrap your App with Amplify Authenticator
+// const AuthenticatedApp = withAuthenticator(App);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//     <Provider store={store}>
+//         <AuthenticatedApp />
+//     </Provider>
+// );
+
+// // Service worker
+// serviceWorker.unregister();
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { routerMiddleware } from 'connected-react-router';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import { Provider } from 'react-redux';
+// import ReduxThunk from 'redux-thunk';
+// import * as serviceWorker from './serviceWorker';
+// import axios from 'axios';
+
+// import App from './components/App';
+
+// import createRootReducer from './reducers';
+// import { doInit } from './actions/auth';
+// import { createHashHistory } from 'history';
+
+// // Amplify setup
+// import { Amplify } from 'aws-amplify';
+// import config from './aws-exports';
+// Amplify.configure(config);
+
+// // Amplify Auth UI
+// import '@aws-amplify/ui-react/styles.css';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
+
+
+
+
+// const history = createHashHistory();
+
+// export function getHistory() {
+//     return history;
+// }
+
+// // Set Axios defaults
+// axios.defaults.baseURL = config.baseURLApi;
+// axios.defaults.headers.common['Content-Type'] = "application/json";
+// const token = localStorage.getItem('token');
+// if (token) {
+//     axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+// }
+
+// export const store = createStore(
+//     createRootReducer(history),
+//     compose(
+//         applyMiddleware(
+//             routerMiddleware(history),
+//             ReduxThunk
+//         ),
+//     )
+// );
+
+// // Init Redux auth state
+// store.dispatch(doInit());
+
+// // Wrap your App with Amplify Authenticator
+// const AuthenticatedApp = withAuthenticator(App);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//     <Provider store={store}>
+//         <AuthenticatedApp />
+//     </Provider>
+// );
+
+// // Service worker
+// serviceWorker.unregister(); 
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { routerMiddleware } from 'connected-react-router';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import { Provider } from 'react-redux'
+// import ReduxThunk from 'redux-thunk'
+// import * as serviceWorker from './serviceWorker';
+// import axios from 'axios';
+
+// import App from './components/App';
+// import config from './config';
+// import createRootReducer from './reducers';
+// import { doInit } from './actions/auth';
+// import { createHashHistory } from 'history';
+
+// const history = createHashHistory();
+
+// export function getHistory() {
+//     return history;
+// }
+
+// axios.defaults.baseURL = config.baseURLApi;
+// axios.defaults.headers.common['Content-Type'] = "application/json";
+// const token = localStorage.getItem('token');
+// if (token) {
+//     axios.defaults.headers.common['Authorization'] = "Bearer " + token;
+// }
+
+// export const store = createStore(
+//     createRootReducer(history),
+//     compose(
+//         applyMiddleware(
+//             routerMiddleware(history),
+//             ReduxThunk
+//         ),
+//     )
+// );
+
+// store.dispatch(doInit());
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// root.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>,
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
